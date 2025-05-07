d557ddff924e3e11db3105351f23ddfc2d513b76
Dowa1511 <dowatandekar@gmail.com>
2025-05-07 18:54:23 +0530

    Create 12.scala

---
+++ 12.scala
@@ -0,0 +1,22 @@
+/*Scala program to print your name*/
+object ExPrintName {
+def main(args: Array[String]) {
+println("My name is Mike!")
+}
+}
+Sign.scala
+/**Scala program to find a number is positive, negative or positive.*/
+object ExCheckNumber {
+def main(args: Array[String]) {
+/**declare a variable*/
+var number= (-100);
+if(number==0){
+println("number is zero");
+}
+else if(number>0){
+println("number is positive");
+}
+else{
+println("number is negative");
+}
+}
